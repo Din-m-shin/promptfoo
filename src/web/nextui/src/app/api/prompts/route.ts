f0a3217521008a53a48d7569be9d894360430d00
din-m-shin <minseob@polarisoffice.com>
2024-08-09 14:01:32 +0900

    JSON 파일 업로드/조회
    1. JSON 파일 업로드
    Fontend 선택 -> API Route 전달 -> 서버 storage에 저장
    
    2. JSON 파일 목록 조회
    Prompt dialog에서 업로드된 json 파일 조회/선택
    
    3. JSON 파일 내용
    Prompt dialog에서 선택된 json 파일 내용 출력

--- src/web/nextui/src/app/api/prompts/route.ts
+++ src/web/nextui/src/app/api/prompts/route.ts
@@ -1,8 +1,39 @@
 import { getPrompts } from '@/../../../util';
 import { IS_RUNNING_LOCALLY, USE_SUPABASE } from '@/constants';
+import fs from 'fs';
+import { existsSync } from 'fs';
 import { NextResponse } from 'next/server';
+import path from 'path';
+import { getConfigDirectoryPath } from '../../../../../../util/config';
 
 export const dynamic = IS_RUNNING_LOCALLY ? 'auto' : 'force-dynamic';
+
+export async function POST(req: any, res: Response) {
+  const uploadDirectory = path.resolve(getConfigDirectoryPath(true /* createIfNotExists */),'upload_file');
+  console.log('uploadDirectory ', uploadDirectory);
+
+  try {
+    const formData = await req.formData();
+    const file = formData.getAll('files')[0];
+    const filePath = `${uploadDirectory}/${file.name}`;
+
+    // console.log('upload filePath ', filePath);
+
+    // Check if the file already exists
+    if (existsSync(filePath)) {
+      return NextResponse.json({ status: 409, error: "File already exists" });
+    }
+
+    const fileArrayBuffer = await file.arrayBuffer();
+    await fs.writeFileSync(filePath, Buffer.from(fileArrayBuffer));
+
+    // console.log('upload file ', file);
+
+    return NextResponse.json({ status: 200, data: { size: file.size, name: file.name, path: filePath} });
+  } catch (e) {
+    return NextResponse.json({ status: 500, data: e });
+  }
+}
 
 export async function GET(req: Request) {
   if (USE_SUPABASE || IS_RUNNING_LOCALLY) {
