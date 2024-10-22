3de83b2e5cc6ea624cc9b1ba563f675bffa3c597
Michael <michael.l.dangelo@gmail.com>
2024-10-22 12:57:09 -0700

    feat(providers): add support for xAI (Grok) provider (#1967)

--- src/providers.ts
+++ src/providers.ts
@@ -73,6 +73,7 @@ import { VoyageEmbeddingProvider } from './providers/voyage';
 import { WatsonXProvider } from './providers/watsonx';
 import { WebhookProvider } from './providers/webhook';
 import { WebSocketProvider } from './providers/websocket';
+import { createXAIProvider } from './providers/xai';
 import RedteamCrescendoProvider from './redteam/providers/crescendo';
 import RedteamIterativeProvider from './redteam/providers/iterative';
 import RedteamImageIterativeProvider from './redteam/providers/iterativeImage';
@@ -220,6 +221,11 @@ export async function loadApiProvider(
     });
   } else if (providerPath.startsWith('togetherai:')) {
     ret = createTogetherAiProvider(providerPath, {
+      config: providerOptions,
+      env: context.env,
+    });
+  } else if (providerPath.startsWith('xai:')) {
+    ret = createXAIProvider(providerPath, {
       config: providerOptions,
       env: context.env,
     });
